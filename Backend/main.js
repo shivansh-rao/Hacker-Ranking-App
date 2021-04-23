24b60ee379fc3b34a5182a0edc68944d24afb279
shivanshrao123 <67832425+shivanshrao123@users.noreply.github.com>
2021-04-23 13:53:20 +0530

    minor changes

--- Backend/main.js
+++ Backend/main.js
@@ -37,4 +37,6 @@ app.post("/hacker/new", hackerController.addNewHacker);
 
 
 
-app.listen(port, () => console.log(`react app listening at http://localhost:${port}`));
+app.listen(process.env.PORT || 3001, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
