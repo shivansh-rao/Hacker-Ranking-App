5eb732e48eef508f4907675fb9f957130b00ed19
shivanshrao123 <67832425+shivanshrao123@users.noreply.github.com>
2021-04-23 23:51:48 +0530

    few changes1

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const path = require('path');
+const cors = require('cors');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+const dbConnection = require('./Backend/config/dbConnect')
+const ratingSystem = require('./Backend/ratingSystem/ratingSystem')
+const User = require('./Backend/model/User.js');
+const authController = require('./Backend/controllers/auth')
+const hackerController = require('./Backend/controllers/userDetails')
+const customMiddlewares = require('./Backend/middlewares/jwtAuth')
+const UserDashboard = require('./Backend/model/userDashboard');
+
+passport.use(new localStrategy(User.authenticate()));
+
+/*Middewares*/
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/*Routes for authentication*/
+app.post("/login", authController.login);
+app.post("/register", authController.register);
+app.get("/logout", authController.logout);
+
+/*Routes to fetch or add data to Userdashboard Model*/
+app.get("/hackers", customMiddlewares.isLoggedIn, hackerController.hackers);
+app.get("/hacker/:id", customMiddlewares.isLoggedIn, hackerController.hackarDetail);
+app.post("/hackers/topHackers",customMiddlewares.isLoggedIn, hackerController.top3hackarDetail)
+app.get("/hackers/recentlyUpdated",customMiddlewares.isLoggedIn, hackerController.recentlyUpdated)
+app.post("/hacker/new", hackerController.addNewHacker);
+
+
+
+
+app.listen(process.env.PORT || 3001, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
